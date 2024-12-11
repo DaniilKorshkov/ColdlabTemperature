dd794bc468dee1b2811130e2aa1bac44230ab127
Daniil <DaniilKorshkov@cmail.carleton.ca>
2024-12-11 17:57:42 -0500

    add polynomes and live graphs

--- SequentMicroInterface.py
+++ SequentMicroInterface.py
@@ -12,21 +12,24 @@ def ReadTemperature(sensor_number):
     
 def ReadAllTemperatures():
     ret = ""
+    temperaturearray = []
     for sensor_number in js.ReadJSONConfig("RTD_options","currently_processed_ports"):
-        ret = ret+"\t"+str(ReadTemperature(int(sensor_number)))
+        temperature = ReadTemperature(int(sensor_number))
+        temperaturearray.append(float(temperature))
+        ret = ret+"\t"+str(temperature)
     ret = ret.strip("\t")
-    return ret
+    return ret, temperaturearray
         
 
 def RecordTemperatureToLog(filename):
     current_time = datetime.datetime.now().strftime("%Y-%h-%d %H:%M:%S")
-    current_temp = ReadAllTemperatures()
+    current_temp, temperaturearray = ReadAllTemperatures()
     
     handle = open(filename,"a")
     handle.write(f"{current_temp}")
     handle.close()
     
-    
+    return temperaturearray
 
 def ReadVoltage(board_id, sensor_number):
     ret = str(subprocess.run([f"megaind", f"{board_id}", f"uoutrd", f"{sensor_number}"], capture_output=True).stdout)
@@ -37,18 +40,25 @@ def ReadVoltage(board_id, sensor_number):
     
 def ReadAllVoltages():
     ret = ""
+    pressurearray = []
+    polynomes = js.ReadJSONConfig("Polynomes","polynomes")
     for i in range(2):
         for j in range(4):
-                ret = ret+"\t"+str(ReadVoltage(i*4,(j+1)))
+                voltage = ReadVoltage(i*4,(j+1))
+                pressure = ((polynomes[i*4+j])[0])*voltage + ((polynomes[i*4+j])[1])
+                pressurearray.append(pressure)
+                ret = ret+"\t"+str(pressure)
+                
     ret = ret.strip("\t")
-    return ret
+    return ret, pressurearray
 
 
 def RecordVoltageToLog(filename):
-    current_volt = ReadAllVoltages()
+    current_volt, pressurearray = ReadAllVoltages()
     handle = open(filename,"a")
     handle.write(f"{current_volt}")
     handle.close()
+    return pressurearray
     
 
 
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import SequentMicroInterface as sm
 import time
 import datetime
 import CreateNewFile
+import matplotlib.pyplot as plt
+import JSONoperators as js
 
 def main():
     
@@ -17,7 +19,26 @@ def main():
             print("Not a valid integer")
 
 
-
+    temperaturearrays=[]
+    for i in js.ReadJSONConfig("RTD_options","currently_processed_ports"):
+        temperaturearrays.append([])
+    pressurearrays = []
+    for i in range(8):
+        pressurearrays.append([])
+        
+    x = 0
+    x_ax = []
+        
+    
+    tempfig = plt.figure()
+    tempax = tempfig.add_subplot(111)
+    tempfig.show()
+    
+    pressfig = plt.figure()
+    pressax = pressfig.add_subplot(111)
+    pressfig.show()
+    
+    
     while True:
         current_time = (datetime.datetime.now()).strftime("%Y-%h-%d %H:%M:%S")
         
@@ -25,15 +46,40 @@ def main():
         handle.write(f"{current_time}\t\t")
         handle.close()
         
-        sm.RecordTemperatureToLog(filename)
+        temperaturelist = sm.RecordTemperatureToLog(filename)
         handle = open(filename,"a")
         handle.write(f"\t\t")
         handle.close()
-        sm.RecordVoltageToLog(filename)
+        
+        for i in range(len(temperaturelist)):
+            temperaturearrays[i].append(temperaturelist[i])
+        
+        pressurelist = sm.RecordVoltageToLog(filename)
         handle = open(filename,"a")
         handle.write(f"\n")
         handle.close()
+        
+        for i in range(len(pressurelist)):
+            pressurearrays[i].append(pressurelist[i])
+        
+        x = x+1
+        x_ax.append(x)
+        
+        for array in temperaturearrays:
+            tempax.plot(x_ax,array,color="b")
+            
+       
+        for array in pressurearrays:
+            pressax.plot(x_ax,array,color="b")
+            
+            
+        tempfig.canvas.draw()
+        pressfig.canvas.draw()
+        
+        
         time.sleep(interval)
+        
+        
 
 
 
