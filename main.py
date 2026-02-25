0a390e2dccb710a2e4c71b482911432d9d0f9552
daniil <daniilkorshkov@cmail.carleton.ca>
2026-02-25 12:25:58 -0500

    removed unused functionality for graph display inside main.py function

--- main.py
+++ main.py
@@ -23,14 +23,8 @@ def main():
     signal.signal(signal.SIGTERM, kill_handler)
     
     filename = CreateNewFile.MakeNewFile()
-    dodisplaygraphs = js.ReadJSONConfig("RTD_options","showgraphs")
-    entriestodisplay = js.ReadJSONConfig("RTD_options","entriestodisplay")
-    refreshtime = js.ReadJSONConfig("RTD_options","refreshtime")
-    plt_backend = js.ReadJSONConfig("Technical","plt_backend")
-
-    #if dodisplaygraphs == "True":
-    #    plt.switch_backend(plt_backend)
     
+  
 
     while True:
         try:
@@ -48,19 +42,6 @@ def main():
     print(f"To terminate the process, please use Ctrl+C")
 
 
-
-        
-    #if dodisplaygraphs == "True":
-    #    pressurearrays, temperaturearrays = initiate_frame()
-
-
-
-        
-        
-    refreshtick = int(interval/refreshtime)
-    if refreshtick < 1:
-        refreshtick = 1
-    refreshcounter = refreshtick
         
     
 
@@ -70,56 +51,25 @@ def main():
     
     while True:
         current_time = (datetime.datetime.now()).strftime("%Y-%h-%d %H:%M:%S")
-        
-        #handle = open(filename,"a")
-        #handle.write(f"{current_time}\t\t")
-        #handle.close()
-        
         tempertaurelogentry, temperaturelist = sm.ReadAllTemperatures()
-        #temperaturelist = sm.RecordTemperatureToLog(filename)
-        #handle = open(filename,"a")
-        #handle.write(f"\t\t")
-        #handle.close()
-        
-        
         pressurelogentry, pressurelist = sm.ReadAllVoltages()
-        #pressurelist = sm.RecordVoltageToLog(filename)
-        #handle = open(filename,"a")
-        #handle.write(f"\n")
-        #handle.close()
-        
-        if refreshcounter >= refreshtick:
-            refreshcounter = 0
-
-            handle = open(filename, "a")
-            handle.write(f"{current_time}\t\t")
-            for element in temperaturelist:
-                handle.write(f"{element}\t")
-            handle.write("\t")
-            for element in pressurelist:
-                handle.write(f"{element}\t")
-            handle.write("\n")
-            handle.close()
-
-            #sm.RecordTemperatureToLog(tempertaurelogentry, filename)
-            #sm.RecordVoltageToLog(pressurelogentry, filename)
-
-
-
-
-
-        refreshcounter += 1
         
         
-        #if dodisplaygraphs == "True":
-        #    update_frame(temperaturearrays, pressurearrays, temperaturelist, pressurelist)
-            
-            
-            
-            
         
-        #time.sleep(interval)
-        time.sleep(refreshtime)
+
+        handle = open(filename, "a")
+        handle.write(f"{current_time}\t\t")
+        for element in temperaturelist:
+            handle.write(f"{element}\t")
+        handle.write("\t")
+        for element in pressurelist:
+            handle.write(f"{element}\t")
+        handle.write("\n")
+        handle.close()
+
+        
+        
+        time.sleep(interval)
     
         
         
@@ -133,116 +83,3 @@ def kill_handler(*args):
 if __name__ == "__main__":
     main()
 
-
-
-
-def initiate_frame():
-
-
-
-
-    #----------------------------   Produce X and Y arrays to display  ------------------------------------------
-
-
-
-
-    temperaturelegend = []
-    pressurelegend = []
-    
-    
-    temperaturearrays=[]
-    for i in js.ReadJSONConfig(f"RTD_options",f"currently_processed_temperature_ports"):
-        temperaturearrays.append([])
-        temperaturelegend.append([i])
-    pressurearrays = []
-    for i in js.ReadJSONConfig(f"RTD_options",f"currently_processed_voltage_ports"):
-        pressurearrays.append([])
-        pressurelegend.append([i+1])
-    
-    
-    
-    x_ax = []
-    
-    colorlist = ['r','g','b','c','m','y','k','tab:brown']
-    
-    
-    
-        
-    #----------------------------------   Render temperature graph frame   ------------------------------------
-        
-    
-    tempfig = plt.figure()
-    tempax = tempfig.add_subplot(111)
-    tempfig.show()
-
-
-    #----------------------------------   Render pressure graph frame   ------------------------------------
-
-    
-    pressfig = plt.figure()
-    pressax = pressfig.add_subplot(111)
-    pressfig.show()
-
-    return pressurearrays, temperaturearrays
-
-
-
-def update_frame(temperaturearrays, pressurearrays, temperaturelist, pressurelist):
-
-
-    #----------------------------   Produce X and Y arrays to display  ------------------------------------------
-
-
-
-    for i in range(len(temperaturelist)):
-        temperaturearrays[i].append(temperaturelist[i])
-        if len(temperaturearrays[i]) > entriestodisplay:
-            void = (temperaturearrays[i]).pop(0)  #reduce array length to 'entriestodisplay'
-            
-    for i in range(len(pressurelist)):
-        pressurearrays[i].append(pressurelist[i])
-        if len(pressurearrays[i]) > entriestodisplay:
-            void = (pressurearrays[i]).pop(0)  #reduce array length to 'entriestodisplay'
-    
-    
-    x_ax.append(datetime.datetime.now())
-    
-    if len(x_ax) > entriestodisplay:
-        void = x_ax.pop(0)
-        
-        
-    #----------------------------------   Render temperature graph frame   ------------------------------------
-
-
-
-    
-    i = 0
-    tempax.cla()
-    tempax.set_title(f"Temperature (oC vs time)")
-    for array in temperaturearrays:
-        
-        tempax.plot(x_ax,array,color=colorlist[i])
-        
-        i += 1
-
-
-    tempax.legend(temperaturelegend,loc=3)
-    tempfig.canvas.draw()
-
-    
-    #----------------------------------   Render pressure graph frame   ------------------------------------
-
-        
-    j = 0
-    pressax.cla()
-    pressax.set_title(f"Pressure (kPa vs time)")
-    for array in pressurearrays:
-        pressax.plot(x_ax,array,color=colorlist[j])
-        
-        j += 1
-    
-
-    
-    
-    pressax.legend(pressurelegend,loc=3)
-    pressfig.canvas.draw()
