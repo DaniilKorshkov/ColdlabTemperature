e2afc809dcd09e001f6ea661ead3df92371e45ec
daniil <daniilkorshkov@cmail.carleton.ca>
2026-08-18 14:57:49 -0400

    merged update frame to main

--- main.py
+++ main.py
@@ -24,19 +24,172 @@ import matplotlib.ticker as ticker
 from matplotlib.dates import DateFormatter
 import datetime as dt
 
+def temperature_update_frame(i):
+
+    entries_to_display = js.ReadJSONConfig("RTD_options","entriestodisplay")
+    raw_time_array, temperature_arrays, pressure_arrays, amperage_arrays = js.ReadCSV(filename,entries_to_display)
+    tempax.xaxis.set_major_formatter(DateFormatter('%H-%M-%S'))
+    legend_dictionary = js.ReadJSONConfig("Dictionaries","RTD_dictionary")
 
 
+    time_array = [dt.datetime.fromtimestamp(element) for element in raw_time_array]
+   
 
+    tempax.cla()
+    #pressax.clear()
+
+    tempax.set_title(f"Temperature (oC vs time)")
+    colorlist = ['r','g','b','c','m','y','k','tab:brown']
+    i = 0
+
+
+    tempax.set_xlabel(f'Time')
+    tempax.set_ylabel("Temperature oC")
+
+    tempax.set_xscale('linear')
+    tempax.set_yscale('linear')
+
+
+    tempax.xaxis.grid(which='major', color='k', alpha=0.8, linestyle='--', linewidth=1)
+    tempax.yaxis.grid(which='major', color='k', alpha=0.8, linestyle='--', linewidth=1)
+
+    tempax.xaxis.grid(which='minor', color='k', alpha=0.5, linestyle=':', linewidth=0.75)
+    tempax.yaxis.grid(which='minor', color='k', alpha=0.5, linestyle=':', linewidth=0.75)
+
+
+    
+    tempax.tick_params('x', labelrotation=90)
+    
+    temperaturelegend = []
+
+
+    for temperature_array_key in temperature_arrays:
+
+
+       
+
+        tempax.plot(time_array, temperature_arrays[temperature_array_key], color = colorlist[i])
+        try:
+            temperaturelegend.append(legend_dictionary[temperature_array_key])
+        except:
+            temperaturelegend.append(temperature_array_key)
+        i += 1
+    
+
+    tempax.legend(temperaturelegend,loc=3)
+
+def amperage_update_frame(i):
+
+    entries_to_display = js.ReadJSONConfig("RTD_options","entriestodisplay")
+    raw_time_array, temperature_arrays, pressure_arrays, amperage_arrays = js.ReadCSV(filename,entries_to_display)
+    ampax.xaxis.set_major_formatter(DateFormatter('%H-%M-%S'))
+    legend_dictionary = js.ReadJSONConfig("Dictionaries","Amp_dictionary")
+
+
+    time_array = [dt.datetime.fromtimestamp(element) for element in raw_time_array]
+   
+
+    ampax.cla()
+    #pressax.clear()
+
+    ampax.set_title(f"Amperage vs time")
+    colorlist = ['r','g','b','c','m','y','k','tab:brown']
+    i = 0
+
+
+    ampax.set_xlabel(f'Time')
+    ampax.set_ylabel("Amperage")
+
+    ampax.set_xscale('linear')
+    ampax.set_yscale('linear')
+
+
+    ampax.xaxis.grid(which='major', color='k', alpha=0.8, linestyle='--', linewidth=1)
+    ampax.yaxis.grid(which='major', color='k', alpha=0.8, linestyle='--', linewidth=1)
+
+    ampax.xaxis.grid(which='minor', color='k', alpha=0.5, linestyle=':', linewidth=0.75)
+    ampax.yaxis.grid(which='minor', color='k', alpha=0.5, linestyle=':', linewidth=0.75)
+
+
+    
+    ampax.tick_params('x', labelrotation=90)
+    
+    amperagelegend = []
+
+
+    for amperage_array_key in amperage_arrays:
+
+
+       
+
+        ampax.plot(time_array, amperage_arrays[amperage_array_key], color = colorlist[i])
+        try:
+            amperagelegend.append(legend_dictionary[amperage_array_key])
+        except:
+            amperagelegend.append(amperage_array_key)
+        i += 1
+    
+
+    ampax.legend(amperagelegend,loc=3)
+
+
+def pressure_update_frame(i):
+
+    entries_to_display = js.ReadJSONConfig("RTD_options","entriestodisplay")
+    raw_time_array, temperature_arrays, pressure_arrays = js.ReadCSV(filename,entries_to_display)
+    pressax.xaxis.set_major_formatter(DateFormatter('%H-%M-%S'))
+    legend_dictionary = js.ReadJSONConfig("Dictionaries","Press_dictionary")
+
+
+    time_array = [dt.datetime.fromtimestamp(element) for element in raw_time_array]
+   
+
+    pressax.cla()
+    #pressax.clear()
+
+    pressax.set_title(f"Pressure (torr?? vs time)")
+    colorlist = ['r','g','b','c','m','y','k','tab:brown']
+    i = 0
+
+
+    pressax.set_xlabel(f'Time')
+    pressax.set_ylabel("Pressure torr")
+
+    pressax.set_xscale('linear')
+    pressax.set_yscale('linear')
+
+
+    pressax.xaxis.grid(which='major', color='k', alpha=0.8, linestyle='--', linewidth=1)
+    pressax.yaxis.grid(which='major', color='k', alpha=0.8, linestyle='--', linewidth=1)
+
+    pressax.xaxis.grid(which='minor', color='k', alpha=0.5, linestyle=':', linewidth=0.75)
+    pressax.yaxis.grid(which='minor', color='k', alpha=0.5, linestyle=':', linewidth=0.75)
+
+
+    
+    pressax.tick_params('x', labelrotation=90)
+    
+    pressurelegend = []
+
+
+    for pressure_array_key in pressure_arrays:
+
+
+       
+
+        pressax.plot(time_array, pressure_arrays[pressure_array_key], color = colorlist[i])
+        try:
+            pressurelegend.append(legend_dictionary[pressure_array_key])
+        except:
+            pressurelegend.append(pressure_array_key)
+        i += 1
+    
+
+    pressax.legend(pressurelegend,loc=3)
 
 
 
 def initiate_frame():
-
-    
-
-    
-    global filename
-    
 
     
     global tempfig
@@ -66,9 +219,9 @@ def initiate_frame():
 
 
 
-    pressani = FuncAnimation(pressfig, TemperatureScreen.update_frame, interval=1000)
-    tempani = FuncAnimation(tempfig, PressureScreen.update_frame, interval=1000)
-    ampani = FuncAnimation(ampfig, AmperageScreen.update_frame, interval=1000)
+    pressani = FuncAnimation(pressfig, pressure_update_frame, interval=1000)
+    tempani = FuncAnimation(tempfig, temperature_update_frame, interval=1000)
+    ampani = FuncAnimation(ampfig, amperage_update_frame, interval=1000)
 
 
     plt.show()
